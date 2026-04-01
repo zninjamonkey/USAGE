b05306429a07ccf81cde80842076d69ded722739
Anson <100244371+zninjamonkey@users.noreply.github.com>
2026-04-01 16:23:50 -0600

    game scrolling sorta works but is glitchy

--- game.gd
+++ game.gd
@@ -2,10 +2,12 @@ class_name game_tile
 
 var name : String
 var image_file_name : String
+var image : Texture2D
 var categories : Array
 
 func _init(name : String, image_file_name : String, categories : Array) -> void:
 	self.name = name
 	self.image_file_name = image_file_name
 	self.categories = categories
+	self.image = load("res://title_images/" + image_file_name)
 	
--- main.gd
+++ main.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var center_animation_player: AnimationPlayer = $"center tile/AnimationPlayer"
 @onready var right_animation_player: AnimationPlayer = $"right tile/AnimationPlayer"
 @onready var left_animation_player: AnimationPlayer = $"left tile/AnimationPlayer"
+@onready var far_right_animation_player: AnimationPlayer = $"far right tile/AnimationPlayer"
+@onready var far_left_animation_player: AnimationPlayer = $"far left tile/AnimationPlayer"
+
 
 @onready var center_tile: Node2D = $"center tile"
 @onready var right_tile: Node2D = $"right tile"
@@ -15,22 +18,67 @@ var hogwarts = game_tile.new("Hogwarts Legacy", "hogwarts.jpg", [0])
 var portal = game_tile.new("Portal", "portal.png", [0])
 var sports = game_tile.new("Wii Sports", "wiisports.png", [0])
 	
-var title_reel = [minecraft, bopl, portal, sports, hogwarts]
+var title_reel : Array[game_tile] = [minecraft, bopl, portal, sports, hogwarts]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	$"center tile/cover".texture = load("res://title_images/" + portal.image_file_name)
-
+	_update_textures()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("right"):
+	print(title_reel[0].name)
+	if Input.is_action_pressed("left") and not center_animation_player.is_playing():
+		#reset()
 		center_animation_player.play("move_right")
 		right_animation_player.play("move_right")
 		left_animation_player.play("move_right")
-	if Input.is_action_just_pressed("left"):
+		far_left_animation_player.play("move_right")
+		far_right_animation_player.play("move_right")
+		
+		
+	if Input.is_action_pressed("right") and not center_animation_player.is_playing():
+		#reset()
 		center_animation_player.play("move_left")
 		right_animation_player.play("move_left")
 		left_animation_player.play("move_left")
+		far_right_animation_player.play("move_left")
+		far_left_animation_player.play("move_left")
+		
+
+
+func _update_textures() -> void:
+	#$"center tile/cover".texture = load("res://title_images/" + title_reel[0].image_file_name)
+	#$"right tile/cover".texture = load("res://title_images/" + title_reel[1].image_file_name)
+	#$"left tile/cover".texture = load("res://title_images/" + title_reel[-1].image_file_name)
+	#$"far left tile/cover".texture = load("res://title_images/" + title_reel[-2].image_file_name)
+	#$"far right tile/cover".texture = load("res://title_images/" + title_reel[2].image_file_name)
+
+	$"center tile/cover".texture = title_reel[0].image
+	$"right tile/cover".texture = title_reel[1].image
+	$"left tile/cover".texture = title_reel[-1].image
+	$"far left tile/cover".texture = title_reel[-2].image
+	$"far right tile/cover".texture = title_reel[2].image
+	
+
+
+func _on_center_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "move_left":
+		title_reel.push_back(title_reel.pop_front())
+		$"far right tile".show()
+		reset()
+	elif anim_name == "move_right":
+		title_reel.push_front(title_reel.pop_back())
+		$"far left tile".show()
+		reset()
+
+func reset():
+		_update_textures()
+
+		center_animation_player.play("RESET")
+		right_animation_player.play("RESET")
+		left_animation_player.play("RESET")
+		far_right_animation_player.play("RESET")
+		far_left_animation_player.play("RESET")
+		
+		#await get_tree().create_timer(1).timeout
 		
